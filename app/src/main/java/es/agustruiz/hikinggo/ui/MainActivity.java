8ea765bc2ed24ee72e9dd6e2d9efafe5689cccb7
Agustín Ruiz Linares <agustinruizlinares@gmail.com>
2016-08-03 12:27:45 +0200

    [FIX] Move MainActivity to "ui" package

--- app/src/main/java/es/agustruiz/hikinggo/ui/MainActivity.java
+++ app/src/main/java/es/agustruiz/hikinggo/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package es.agustruiz.hikinggo;
+package es.agustruiz.hikinggo.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import es.agustruiz.hikinggo.R;
 
 public class MainActivity extends AppCompatActivity {
 
